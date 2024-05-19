0fc8db0cc65ce54b98d676845f56405fab575a47
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-05-19 22:48:10 +0300

    1.2

---
+++ src/js/render-functions.js
@@ -0,0 +1,41 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export const createMarkupItem = images => {
+  return images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+  <li class="gallery-item">
+    <a class="gallery-link" href="${largeImageURL}">
+      <img
+        class="gallery-image"
+        src="${webformatURL}"
+        alt="${tags}"
+      />
+    </a>
+    <div class="small-content">
+        <span class="text-body-likes">Likes: ${likes}</span>
+        <span class="text-body-views">Views: ${views}</span>
+        <span class="text-body-comments">Comments: ${comments}</span>
+        <span class="text-body-downloads">Downloads: ${downloads}</span>
+    </div>
+  </li>
+`;
+      }
+    )
+    .join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+import { fetchPhotosByQuery } from "./js/pixabay-api";
 
+console.log(fetchPhotosByQuery());
